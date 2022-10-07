20137a29f2beaf5875754d33d5926ced3569fef1
sgl7955 <sgl7955@gmail.com>
2022-10-07 13:46:46 +0900

    데이터 value 결과 계산 및 결과페이지 전환

--- Start/js/start.js
+++ Start/js/start.js
@@ -1,8 +1,69 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
-const endPoint = 12; //12개 질문//
+const result = document.querySelector('#result');
 
-function addAnswer(answerText, qIdx){
+const endPoint = 12; //12개 질문//
+const select = [];
+
+function calResult () {
+    var pointArray = [
+        { name: 'mouse', value: 0, key: 0},
+        { name: 'cow', value: 0, key: 1},
+        { name: 'tiger', value: 0, key: 2},
+        { name: 'rabbit', value: 0, key: 3},
+        { name: 'dragon', value: 0, key: 4},
+        { name: 'snake', value: 0, key: 5},
+        { name: 'horse', value: 0, key: 6},
+        { name: 'sheep', value: 0, key: 7},
+        { name: 'monkey', value: 0, key: 8},
+        { name: 'chick', value: 0, key: 9},
+        { name: 'dog', value: 0, key: 10},
+        { name: 'pig', value: 0, key: 11},
+    ]
+
+    for(let i = 0; i < endPoint; i++) {
+        var target = qnaList[i].a[select[i]];
+        for(let j = 0;  j < target.type.length; j++) {
+            for(let k = 0; k < pointArray.length; k++) {
+                if(target.type[j] === pointArray[k].name) {
+                    pointArray[k].value += 1;
+                }
+            }
+        }
+
+    }
+
+    var resultArray = pointArray.sort(function (a, b){
+        if(a.value > b.value) {
+            return -1;
+        }
+        if(a.value < b.value) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(resultArray)
+    let resultword = resultArray[0].key;
+    return resultword;
+}
+
+function goResult() {
+    qna.style.WebkitAnimation = 'fadeOut 1s'; //애니메이션 1초동안 지속//
+    qna.style.animation = 'fadeOut 1s';
+    setTimeout(() => {  //애니메이션이 실행되고 메인페이지를 none으로 바꿈//
+        result.style.WebkitAnimation = 'fadeIn 1s';
+        result.style.animation = 'fadeIn 1s';
+        setTimeout(() => {  
+            qna.style.display = 'none';
+            result.style.display = 'block';
+        }, 450)})
+
+        console.log(select);
+        calResult();
+    }
+
+
+function addAnswer(answerText, qIdx, idx){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button'); //answer변수에 버튼 만들기//
     answer.classList.add('answerList'); //answerList 클래스 추가//
@@ -21,6 +82,7 @@ function addAnswer(answerText, qIdx){
             children[i].style.animation = 'fadeOut 0.5s';        
         }
         setTimeout(() => {
+            select[qIdx] = idx; //몇번째 질문에서 몇번째 버튼 클릭//
             for(let i = 0; i < children.length; i++){
                 children[i].style.display = 'none';
             }
@@ -29,10 +91,15 @@ function addAnswer(answerText, qIdx){
     }, false);
 }
 function goNext(qIdx){
+    if(qIdx === endPoint) {
+        goResult();
+        return;
+    }
+
     var q = document.querySelector('.qBox');
     q.innerHTML = qnaList[qIdx].q; //innerHTML : 요소 내 html 가져옴,설정//
     for(let i in qnaList[qIdx].a){
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
     var status = document.querySelector('.statusBar')
     status.style.width = (100/endPoint) * (qIdx+1) + '%';
